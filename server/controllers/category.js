12a228d4405e28b3cae41f5218e7be0bbe931f92
bhuwan <bkspoudel@gmail.com>
2023-09-13 15:21:34 +0200

    find single category with the help of slug as id

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -21,3 +21,77 @@ export const createCategory = async(req, res)=>{
         return res.json({error: error.message});
     }
 }
+
+
+export const updateCategory = async(req, res)=>{
+    try{
+        const { name } = req.body;
+        const existingCategory = await Category.findOne({name});
+        if(existingCategory){
+            return res.status(400).json({error: "Category already exists with this name"});
+        }
+        const category = new Category({
+            name,
+            slug: slugify(name), // cloth man ---> cloth-man
+        })
+
+        await category.save();
+
+        return res.status(200).json({message: 'category created....'});
+    }catch(err){
+        return res.json({error: error.message});
+    }
+}
+
+export const deleteCategory = async(req, res)=>{
+    try{
+        const { name } = req.body;
+        const existingCategory = await Category.findOne({name});
+        if(existingCategory){
+            return res.status(400).json({error: "Category already exists with this name"});
+        }
+        const category = new Category({
+            name,
+            slug: slugify(name), // cloth man ---> cloth-man
+        })
+
+        await category.save();
+
+        return res.status(200).json({message: 'category created....'});
+    }catch(err){
+        return res.json({error: error.message});
+    }
+}
+
+export const getCategory = async(req, res)=>{
+    try{
+        const { slug } = req.params;
+        const singleCategory = await Category.findOne({ slug });
+        if(!singleCategory){
+            return res.status(400).json({error: "no Category found.."});
+        }
+        return res.status(200).json(singleCategory);
+    }catch(err){
+        return res.json({error: err.message});
+    }
+}
+
+export const getCategories = async(req, res)=>{
+    try{
+        const { name } = req.body;
+        const existingCategory = await Category.findOne({name});
+        if(existingCategory){
+            return res.status(400).json({error: "Category already exists with this name"});
+        }
+        const category = new Category({
+            name,
+            slug: slugify(name), // cloth man ---> cloth-man
+        })
+
+        await category.save();
+
+        return res.status(200).json({message: 'category created....'});
+    }catch(err){
+        return res.json({error: error.message});
+    }
+}
--- server/routes/category.js
+++ server/routes/category.js
@@ -2,13 +2,18 @@ import { Router } from "express";
 
 import { runValidation } from "../validations/index.js";
 import { categoryValidator } from "../validations/category.js";
-import { createCategory } from "../controllers/category.js";
+import { createCategory, updateCategory, getCategory, deleteCategory, getCategories } from "../controllers/category.js";
 
 const categoryRoute = Router();
 
 
 
 categoryRoute.post("/", createCategory);
+categoryRoute.get("/:slug", getCategory);
+categoryRoute.get("/", getCategories);
+categoryRoute.put("/:slug", updateCategory);
+categoryRoute.delete("/:slug", deleteCategory);
+
 
 
 export default categoryRoute;
